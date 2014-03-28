2e6c80ee569f7b4dcb0bc4b2dfe1fca9da796eb7
Bernd Reinhold <reinhol1@fnal.gov>
2014-03-27 22:49:18 -0500

    changed from 10^6 to 10^7 optical photons per default. Changed from FusedSilicaUVAbs to GridSteelRindScale. These are minor changes.

--- MCTuningCode/main_script.py
+++ MCTuningCode/main_script.py
@@ -5,7 +5,8 @@ import os
 #os._exit(-1) to exit unconditionally
 import string
 sys.argv.append( '-b' ) #batch mode, or call as: python main_script.py -b
-import MCTuning_FusedSilicaUVAbs
+#import MCTuning_FusedSilicaUVAbs as MCT
+import MCTuningBase as MCT
 #python main_script.py &> FusedSilicaUVAbs_output.log &
 
 #ToDo:
@@ -14,7 +15,8 @@ import MCTuning_FusedSilicaUVAbs
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 def main():
-    x = MCTuning_FusedSilicaUVAbs.MCTuning_FusedSilicaUVAbs()
+    #x = MCT.MCTuning_FusedSilicaUVAbs()
+    x = MCT.MCTuning()
     print x.__doc__
     x.loop_1par() #switch on and off certain analysis steps in the MCTuningBase.py
 
