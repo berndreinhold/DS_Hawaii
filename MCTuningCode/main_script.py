9f15b6030d2d5f58c00d0af38805ea216e8ecde0
Bernd Reinhold <reinhol1@fnal.gov>
2014-03-30 00:18:18 -0500

    mv files, avoid the '_v1'-suffix (or '_v2', etc.). Produce both Krypton as well as optical photons.

--- MCTuningCode/main_script.py
+++ MCTuningCode/main_script.py
@@ -7,7 +7,7 @@ import string
 sys.argv.append( '-b' ) #batch mode, or call as: python main_script.py -b
 #import MCTuning_FusedSilicaUVAbs as MCT
 import MCTuningBase as MCT
-#python main_script.py &> FusedSilicaUVAbs_output.log &
+#python main_script.py |& tee /scratch/darkside/reinhol1/GridSteelRindScale/83mKr_GridSteelRindScale_output.log &
 
 #ToDo:
 #- submit jobs to Fermigrid
@@ -16,9 +16,11 @@ import MCTuningBase as MCT
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 def main():
     #x = MCT.MCTuning_FusedSilicaUVAbs()
-    x = MCT.MCTuning()
-    print x.__doc__
+    x = MCT.MCTuning("83mKr", 1e5)
     x.loop_1par() #switch on and off certain analysis steps in the MCTuningBase.py
+
+    x = MCT.MCTuning("Optical", 1e6)
+    x.loop_1par() 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if __name__ == "__main__":
