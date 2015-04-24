7c1fc920a5f07a5627b2a6b6d0be5ef6c4dfa7fe
Bernd Reinhold <reinhol1@fnal.gov>
2015-04-23 22:14:59 -0500

    Added new variables to output ntuple. Added output on number of events firing a trigger (without involving clusters) -> therefore no bias in the fraction from multi-counting from multi-clusters.

--- DS_MCProd/chainThem_moredetails.C
+++ DS_MCProd/chainThem_moredetails.C
@@ -9,48 +9,83 @@ using namespace std;
 
 int main(){
 
-  string dir="/pnfs/darkside/scratch/users/reinhol1/CalibData/";
+  string dir="/pnfs/darkside/scratch/users/reinhol1/CalibData/archive/CopperRings_SourceHolder_opticsOn_center+26mm";
+  //string dir="/pnfs/darkside/scratch/users/reinhol1/CalibData/";
   //string outfile="Co57_afterCopperRings_center+2cm.root";
   //string label="center-2cm";
   //string label="center+2cm";
   //string label="center";
   string label="CopperRings_SourceHolder_opticsOn";
-  string zposition="center+26mm";
-  string isotope="Co57_optics"; //macro name
+  string zposition="center+40mm";
+  //string isotope="Co57_optics"; //macro name
+  string isotope="Th232"; //macro name, Cs137, Ba133, Th232
 
   cout << "label: " << label << endl;
+  cout << "zposition: " << zposition << endl;
+  cout << "isotope: " << isotope << endl;
   string outfile=Form("%s_%s_more_details_%s.root",isotope.c_str(), label.c_str(),zposition.c_str());
 
   TChain *ch=new TChain("dstree");
-  for(int i=1000;i<1012;++i){
+  //for(int i=1000;i<1012;++i){
+  for(int i=950;i<980;++i){
     //    ch->Add(Form("%s/Co57_afterCopperRings_center+2cm_RandomSeed_%d.root", dir.c_str(), i));
-    ch->Add(Form("%s/%s_%s_%s_RandomSeed_%d.root", dir.c_str(),isotope.c_str(), label.c_str(),zposition.c_str(), i));
+    ch->Add(Form("%s/%s_%s_%sRandomSeed_%d.root", dir.c_str(),isotope.c_str(), label.c_str(),zposition.c_str(), i));
   }
-  cout << "entries in chain:" << ch->GetEntries() << endl;
 
-  TNtuple *tN=new TNtuple("tN","t_drift (in mm or us) and single cluster selection", "ev:nclus:cl_npe:t_drift:cl_x:cl_y:cl_z");
+  int nentries=ch->GetEntries();
+  int trigger=ch->GetEntries("trigger>0");
+  int tr_ss=ch->GetEntries("trigger>0 && nclus==1");
+
+  cout << "entries in chain: " << nentries << endl;
+  cout << "entries with trigger: " << trigger << "(" << trigger*1.0/nentries << ")" <<  endl;
+  cout << "entries with trigger && single scatter/cluster: " << tr_ss << "(" << tr_ss*1.0/nentries << ")" <<  endl;
+
+  TNtuple *tN=new TNtuple("tN_clusters","info per cluster - t_drift (in mm or us) and single cluster selection", "ev:nclus:npe:cl_npe:t_drift:cl_x:cl_y:cl_z:trigger:ene0:nentries"); //ene0: initial truth energy spectrum (understand branching ratio), nentries: total number of events simulated -> useful for Trigger studies
+  //  TNtuple *tNevents=new TNtuple("tN_events","info per event", "ev:npe:nclus:trigger");
+
+  /*
+  ch->Draw("ev:npe:nclus:trigger","nclus>0","goff"); //one common n-tp
+  cout << "selected entries: " << ch->GetSelectedRows() << endl;
+  for(int i=0;i<ch->GetSelectedRows();++i){
+    if(i%100000==0) cout << i << ",";
+    tNevents->Fill(ch->GetV1()[i],ch->GetV2()[i], ch->GetV3()[i], ch->GetV4()[i]);
+  }
+  cout << endl;
+  */
+
 
   //for 200 V/cm: max t_drift: 373.3, drift velocity 0.938 mm/us (slides by Erin), 17.5 cm is the half length of the TPC active volume in MC, 3.5 is the TPC offset 
   string var="373.3/2.5*(1-cl_z/(17.5-3.5))";
 
   TCut tC="cl_npe>1";
-  ch->Draw(Form("ev:nclus:cl_npe:%s",var.c_str()),tC,"goff"); //one common n-tp
+  ch->Draw("ev:nclus:npe",tC,"goff"); //one common n-tp
   cout << "selected entries: " << ch->GetSelectedRows() << endl;
   std::vector<float> v1;
   std::vector<float> v2;
   std::vector<float> v3;
-  std::vector<float> v4;
 
   for(int i=0;i<ch->GetSelectedRows();++i){
     v1.push_back(ch->GetV1()[i]);
     v2.push_back(ch->GetV2()[i]); 
-    v3.push_back(ch->GetV3()[i]);
-    v4.push_back(ch->GetV4()[i]);
+    v3.push_back(ch->GetV3()[i]); 
   }
 
-  ch->Draw("cl_x:cl_y:cl_z",tC,"goff"); //one common n-tp
+  std::vector<float> v4;
+  std::vector<float> v5;
+  std::vector<float> v6;
+
+  ch->Draw(Form("cl_npe:%s:cl_x",var.c_str()),tC,"goff"); //one common n-tp
+  cout << "selected entries: " << ch->GetSelectedRows() << endl;
   for(int i=0;i<ch->GetSelectedRows();++i){
-    tN->Fill(v1.at(i), v2.at(i), v3.at(i), v4.at(i),ch->GetV1()[i],ch->GetV2()[i], ch->GetV3()[i]);
+    v4.push_back(ch->GetV1()[i]);
+    v5.push_back(ch->GetV2()[i]);
+    v6.push_back(ch->GetV3()[i]);
+  }
+
+  ch->Draw("cl_y:cl_z:trigger:ene0",tC,"goff"); //one common n-tp
+  cout << "selected entries: " << ch->GetSelectedRows() << endl;
+  for(int i=0;i<ch->GetSelectedRows();++i){
+    tN->Fill(v1.at(i), v2.at(i), v3.at(i), v4.at(i),v5.at(i), v6.at(i), ch->GetV1()[i],ch->GetV2()[i], ch->GetV3()[i], ch->GetV4()[i], nentries);
   }
 
   /*
@@ -67,6 +102,7 @@ int main(){
   //h2->SetTitle(Form("one cluster only, %s",label.c_str()));
 
   tN->Write();
+  //tNevents->Write();
   //h1->Write();
   //h2->Write();
   fOut->Close();
