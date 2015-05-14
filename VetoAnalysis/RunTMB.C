d2f058462426a4493a7cef7631f9b38afa037296
Bernd Reinhold <reinhol1@fnal.gov>
2015-05-14 03:43:50 -0500

    added code for the systematic study on how the reconstruction threshold and algorithm affects the low energy alpha only peak.

--- VetoAnalysis/RunTMB.C
+++ VetoAnalysis/RunTMB.C
@@ -1,6 +1,12 @@
 RunTMB(){
-  //EnergyCut_withVeto();
-  AllEnergies_withVeto();
+  // EnergyCut_withVeto();
+  // AllEnergies_withVeto();
+  SystematicsStudy("am00");
+  SystematicsStudy("am10", 11155);
+  SystematicsStudy("am20", 11155);
+  SystematicsStudy("am30", 11155);
+  SystematicsStudy("am40", 11155);
+  SystematicsStudy("am50", 11155);
 }
 
 //useful for deltaT studies
@@ -44,6 +50,25 @@ void AllEnergies_withVeto(){
   string infile_name="DSTAwayData_PPO_15.root";
   t.Input(infile_name,input_path);
   string outfile_name=Form("DSTAwayData_PPO_15_allEnergies_AfterPulsesRejected_%d.root",timeCutAfterPrompt);
+  //string outfile_name=Form("DSTAwayData_PPO_15_allEnergies_AfterPulsesRejected_withVeto10PE_15us_%d.root",timeCutAfterPrompt);
+  t.Output(outfile_name); //use output_path=input_path;
+  t.Loop();       // Loop on all entries
+
+}
+
+
+void SystematicsStudy(string label="am30", int run=11155){
+  gROOT->Reset();
+  gROOT->ProcessLine(".L TMBAlphaOnly_DSTAwayData.C");
+  //TMBAlphaOnly_DSTAwayData t(30, 200, 310);
+  int timeCutAfterPrompt=15;
+  TMBAlphaOnly_DSTAwayData t(timeCutAfterPrompt, 0, 1e6);
+  //t.SetVetoParameters(10, 1e6, 15); //optional, more stringent
+  t.SetVetoParameters(200, 1e6, 15); //optional, just the alpha+gamma and above
+  string input_path="/scratch/darkside/reinhol1/Veto/DSTAwayData/";
+  string infile_name=Form("DST_%s_Run0%d.root", label.c_str(),run);
+  t.Input(infile_name,input_path);
+  string outfile_name=Form("DST_%s_Run0%d_allEnergies_AfterPulsesRejected_%d.root",label.c_str(), run, timeCutAfterPrompt);
   //string outfile_name=Form("DSTAwayData_PPO_15_allEnergies_AfterPulsesRejected_withVeto10PE_15us_%d.root",timeCutAfterPrompt);
   t.Output(outfile_name); //use output_path=input_path;
   t.Loop();       // Loop on all entries
